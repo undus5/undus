6b16be158db288a848d6180eb8b65bf21bd8b0c1
undus5 <undus5.curtsy947@passinbox.com>
2024-11-26 12:22:30 +0800

    fix build script

--- build.sh
+++ build.sh
@@ -5,12 +5,11 @@ _theme=themes/hugo-pure
 
 cd ${_sdir}
 
-if [[ ! -d ${_theme} ]]; then
-    mkdir -p themes
-    curl -SLO https://github.com/undus5/hugo-pure/archive/refs/heads/main.zip
-    unzip main.zip
-    mv hugo-pure-main ${_theme}
-    rm main.zip
-fi
+[[ -d ${_theme} ]] && rm -r themes
+mkdir -p themes
+curl -SLO https://github.com/undus5/hugo-pure/archive/refs/heads/main.zip
+unzip main.zip
+mv hugo-pure-main ${_theme}
+rm main.zip
 
 hugo --minify $@
